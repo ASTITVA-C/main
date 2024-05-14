4dd22e4313e200b70eb811696bfea3bd670c5ce4
ASTITVA-C <167618674+ASTITVA-C@users.noreply.github.com>
2024-05-14 10:10:44 +0000

    done

--- README.md
+++
@@ -1,3 +0,0 @@
-THIS IS MY UBER CLONE 
-I have made this without using a laptop so the elements will not match exactly but I will try to do my best .
-Also I dont know javascript right now I am learning it right now so it wouldnt be very interactive . 
--- UBER/UBER/asgi.py
+++ UBER/UBER/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fr project.
+ASGI config for UBER project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fr.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UBER.settings")
 
 application = get_asgi_application()
